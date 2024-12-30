e0af61f37db4f17ced1873133986cacd2151fbc2
Phuong0811-tech <phuongot08112005@gmail.com>
2024-12-30 12:06:18 +0700

    phuong

---
+++ hssv.sql
@@ -0,0 +1,49 @@
+﻿-- Bảng Khoa
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(100)
+);
+
+-- Bảng Lớp học
+CREATE TABLE classes (
+    class_id INT PRIMARY KEY,
+    class_name VARCHAR(100),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+-- Bảng Sinh viên
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    dob DATE,
+    gender VARCHAR(10),
+    address VARCHAR(255),
+    phone VARCHAR(20),
+    email VARCHAR(100),
+    class_id INT,
+    department_id INT,
+    enrollment_year YEAR,
+    FOREIGN KEY (class_id) REFERENCES classes(class_id),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+-- Bảng Môn học
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(100),
+    credits INT,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+-- Bảng Điểm
+CREATE TABLE grades (
+    grade_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT,
+    score DECIMAL(5,2),
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
